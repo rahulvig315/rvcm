3897cc4f0143d0d357476c2749296fe32dda1b59
rahulvig315 <rahulvig315@gmail.com>
2023-08-07 12:27:51 -0400

    refactor(aside): DRY

--- src/components/(shared)/Aside.tsx
+++ src/components/(shared)/Aside.tsx
@@ -6,19 +6,23 @@ import { Routes } from '@/constants';
 import Link from 'next/link';
 
 
+const checkIfRouteActive = (active: string, route: Routes): string => {
+  return active === route && 'active' || '';
+}
+
 function Aside() {
   const active = usePathname();
   return (
-    <aside className='w-1/6 bg-[#111] h-full rounded-lg hidden md:flex flex-col justify-center place-items-center text-left mt-10 sticky min-w-fit text-sm'>
-        <Link className={`${active === Routes.Dashboard ? "active" : ""}`} href={Routes.Dashboard}>Dashboard</Link>
-        <Link className={`${active === Routes.Customers ? "active" : ""}`} href={Routes.Customers} >Customers</Link>
-        <Link className={`${active === Routes.Analytics ? "active" : ""}`} href={Routes.Analytics} >Analytics</Link>
-        <Link className={`${active === Routes.Schedule ? "active" : ""}`} href={Routes.Schedule} >Schedule</Link>
-        <Link className={`${active === Routes.Finances ? "active" : ""}`} href={Routes.Finances}>Finances</Link>
-        <Link className={`${active === Routes.Leads ? "active" : ""}`} href={Routes.Leads} >Leads</Link>
-        <Link className={`${active === Routes.Tasks ? "active" : ""}`} href={Routes.Tasks} >Tasks</Link>
-        <Link className={`${active === Routes.Logs ? "active" : ""}`} href={Routes.Logs} >Logs</Link>
-        <Link className={`${active === Routes.Chat ? "active" : ""}`} href={Routes.Chat}>Chat</Link>
+    <aside >
+      <Link className={checkIfRouteActive(active, Routes.Dashboard)} href={Routes.Dashboard}>Dashboard</Link>
+      <Link className={checkIfRouteActive(active, Routes.Customers)} href={Routes.Customers} >Customers</Link>
+      <Link className={checkIfRouteActive(active, Routes.Analytics)} href={Routes.Analytics} >Analytics</Link>
+      <Link className={checkIfRouteActive(active, Routes.Schedule)} href={Routes.Schedule} >Schedule</Link>
+      <Link className={checkIfRouteActive(active, Routes.Finances)} href={Routes.Finances}>Finances</Link>
+      <Link className={checkIfRouteActive(active, Routes.Leads)} href={Routes.Leads} >Leads</Link>
+      <Link className={checkIfRouteActive(active, Routes.Tasks)} href={Routes.Tasks} >Tasks</Link>
+      <Link className={checkIfRouteActive(active, Routes.Logs)} href={Routes.Logs} >Logs</Link>
+      <Link className={checkIfRouteActive(active, Routes.Chat)} href={Routes.Chat}>Chat</Link>
     </aside>
   )
 }
