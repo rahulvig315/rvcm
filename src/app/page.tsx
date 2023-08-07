5c7c99ca1437f2e9f98d130be710ae605d7bcdda
rahulvig315 <rahulvig315@gmail.com>
2023-08-07 12:39:21 -0400

    style(tailwind): extract home classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Logo from "../components/(shared)/Logo";
 import { Login } from "../components/(auth)/Login";
 
+const homeClasses = "side-by-side";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen gap-16 p-24 md:flex-row fle">
-      <Logo/>
+    <main className={homeClasses}>
+      <Logo />
       <Login />
     </main>
   )
